a039c82342eafc6e297233761f6d34a539bd2f40
Chris Troutner <chris.troutner@gmail.com>
2021-09-26 17:05:17 -0700

    fix(bootstrap): Updating bootstrap peers

--- config/bootstrap-circuit-relays.js
+++ config/bootstrap-circuit-relays.js
@@ -8,15 +8,15 @@ const BOOTSTRAP_BROWSER_CRs = [
   {
     name: 'ipfs-service-provider.fullstackcash.nl',
     multiaddr:
-      '/dns4/ipfs-service-provider.fullstackcash.nl/tcp/443/wss/ipfs/QmedLCUDSSvsjfPt9rDm65drNL7Dzu1mk1JCRxu9yuxgLL',
+      '/dns4/ipfs-service-provider.fullstackcash.nl/tcp/443/wss/ipfs/QmU6nhzDNipg4kpKjfvpZqdXvfXwoNWSL9jvf29fXj5WKn',
     connected: false,
     ipfsId: 'QmedLCUDSSvsjfPt9rDm65drNL7Dzu1mk1JCRxu9yuxgLL',
     isBootstrap: true
   },
   {
-    name: 'p2wdb-wss.fullstackcash.nl',
+    name: 'psf-cr-US-cali',
     multiaddr:
-      '/dns4/p2wdb-wss.fullstackcash.nl/tcp/443/wss/p2p/QmNV2rCHSjrMR4p8E61b7aTMdkH14Re8L7o7GJ6tyHD6C7',
+      '/dns4/ipfs-cr.fullstackcash.nl/tcp/443/wss/p2p/QmXvT9Tn5VbFU4kGobEt8XS2tsyfjohAzYNa4Wmv8P6pzV',
     connected: false,
     ipfsId: 'QmNV2rCHSjrMR4p8E61b7aTMdkH14Re8L7o7GJ6tyHD6C7',
     isBootstrap: true
@@ -25,19 +25,19 @@ const BOOTSTRAP_BROWSER_CRs = [
 
 const BOOTSTRAP_NODE_CRs = [
   {
-    name: 'fullstack-p2wdb-cali',
+    name: 'ipfs-service-provider.fullstackcash.nl',
     multiaddr:
-      '/ip4/137.184.13.92/tcp/5668/p2p/Qma4iaNqgCAzA3HqNNEkKZzqWhCMnjt19TEHLu8TKhHhRK',
+      '/ip4/162.55.59.102/tcp/5268/p2p/QmU6nhzDNipg4kpKjfvpZqdXvfXwoNWSL9jvf29fXj5WKn',
     connected: false,
-    ipfsId: 'Qma4iaNqgCAzA3HqNNEkKZzqWhCMnjt19TEHLu8TKhHhRK',
+    ipfsId: 'QmedLCUDSSvsjfPt9rDm65drNL7Dzu1mk1JCRxu9yuxgLL',
     isBootstrap: true
   },
   {
-    name: 'ipfs-service-provider.fullstackcash.nl',
+    name: 'fullstack-p2wdb-cali',
     multiaddr:
-      '/ip4/157.90.28.11/tcp/4001/p2p/QmedLCUDSSvsjfPt9rDm65drNL7Dzu1mk1JCRxu9yuxgLL',
+      '/ip4/137.184.13.92/tcp/5268/p2p/QmXvT9Tn5VbFU4kGobEt8XS2tsyfjohAzYNa4Wmv8P6pzV',
     connected: false,
-    ipfsId: 'QmedLCUDSSvsjfPt9rDm65drNL7Dzu1mk1JCRxu9yuxgLL',
+    ipfsId: 'Qma4iaNqgCAzA3HqNNEkKZzqWhCMnjt19TEHLu8TKhHhRK',
     isBootstrap: true
   }
   // {
