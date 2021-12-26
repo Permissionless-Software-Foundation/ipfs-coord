7d0aef5071ceb83e9e8429aaa5bebb4b9c7e74ad
Chris Troutner <chris.troutner@gmail.com>
2021-12-26 07:46:49 -0800

    Upgrading ipfs and orbit-db

--- examples/start-max-config.js
+++ examples/start-max-config.js
@@ -3,30 +3,31 @@
   maximum amount of configuration options.
 */
 
-const IPFS = require('@chris.troutner/ipfs')
-const BCHJS = require('@psf/bch-js')
+// const IPFS = require('@chris.troutner/ipfs')
+const IPFS = require("ipfs");
+const BCHJS = require("@psf/bch-js");
 // const IpfsCoord = require('ipfs-coord')
-const IpfsCoord = require('../index')
+const IpfsCoord = require("../index");
 
 const announceJson = {
-  '@context': 'https://schema.org/',
-  '@type': 'WebAPI',
-  name: 'ipfs-coord-example',
-  version: '1.0.0',
-  protocol: 'none',
-  description: 'This is an example IPFS node using ipfs-coord.',
+  "@context": "https://schema.org/",
+  "@type": "WebAPI",
+  name: "ipfs-coord-example",
+  version: "1.0.0",
+  protocol: "none",
+  description: "This is an example IPFS node using ipfs-coord.",
   documentation:
-    'https://github.com/Permissionless-Software-Foundation/ipfs-coord',
+    "https://github.com/Permissionless-Software-Foundation/ipfs-coord",
   provider: {
-    '@type': 'Organization',
-    name: 'Permissionless Software Foundation',
-    url: 'https://PSFoundation.cash'
+    "@type": "Organization",
+    name: "Permissionless Software Foundation",
+    url: "https://PSFoundation.cash"
   }
-}
+};
 
 // Ipfs Options
 const ipfsOptions = {
-  repo: './.ipfsdata',
+  repo: "./.ipfsdata",
   start: true,
   config: {
     relay: {
@@ -47,26 +48,26 @@ const ipfsOptions = {
     },
     offline: true
   }
-}
+};
 
-async function start () {
+async function start() {
   // Create an instance of bch-js and IPFS.
-  const bchjs = new BCHJS()
-  const ipfs = await IPFS.create(ipfsOptions)
+  const bchjs = new BCHJS();
+  const ipfs = await IPFS.create(ipfsOptions);
 
   // Pass bch-js and IPFS to ipfs-coord when instantiating it.
   const ipfsCoord = new IpfsCoord({
     ipfs,
     bchjs,
-    type: 'node.js',
+    type: "node.js",
     privateLog: console.log, // Replace with your own log.
     isCircuitRelay: false, // Set to true to provide Circuit Relay functionality.
-    apiInfo: 'Link to API documentation if applicable',
+    apiInfo: "Link to API documentation if applicable",
     announceJsonLd: announceJson,
     debugLevel: 2
-  })
+  });
 
-  await ipfsCoord.start()
-  console.log('IPFS and the coordination library is ready.')
+  await ipfsCoord.start();
+  console.log("IPFS and the coordination library is ready.");
 }
-start()
+start();
